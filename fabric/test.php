e99ceea96a21d501aa7c16601f1b5252d3ec3653
darshakshah <dhirendra.rathod@drcinfotech.com>
2016-07-18 12:12:16 +0530

    ALL files committed
    ALL files committed

---
+++ fabric/test.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Canvas Demo</title>
+        <script type="text/javascript" src="assets/js/fabric.js"></script>
+        <script type="text/javascript" src="assets/js/jquery.js"></script>
+    </head>
+    <body>
+        <div style="width: 300px;float: left;">
+            <canvas id="canvas" class="canvas" height="300" width="300" style="border: 1px solid #000;"></canvas>
+        </div>
+        <div style="float: left;margin-left: 10px;">
+            <h3>Control</h3>
+            Enter Text: <input type="text" class="text-type">
+            <input type="button" value="Insert" class="insert">
+        </div>
+        <script type="text/javascript">
+            
+        </script>
+    </body>
+</html>
